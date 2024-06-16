284b428d7e6bfefdac0bdce044300e4d51ac36dd
Jay Mistry <jay.mistry456@yahoo.com>
2024-06-16 17:12:28 -0300

    quick sort based on pivot as the first index

--- SortingAlgorithms/QuickSort.py
+++ SortingAlgorithms/QuickSort.py
@@ -1,16 +1,23 @@
-def partition(arr, start, end):
-    pivot = arr[end]
-    partition_index = start
-    for i in range(start, end):
-        if arr[i] <= pivot:
-            arr[i], arr[partition_index] = arr[partition_index], arr[i]
-            partition_index = partition_index + 1
-    pivot, arr[partition_index] = arr[partition_index], pivot
-    return partition_index
+def partition(arr, low, high):
+    pivot = arr[low]
+    i = low + 1
+    j = high
+    while True:
+        while i <= j and arr[i] <= pivot:
+            i = i + 1
+        while i <= j and arr[j] > pivot:
+            j = j - 1
+        if j < i:
+            break
+        else:
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[j], arr[low] = arr[low], arr[j]
+    return j
 
 
-def quick_sort(arr, start, end):
-    if start < end:
-        partition_index = partition(arr, start, end)
-        quick_sort(arr, start, partition_index - 1)
-        quick_sort(arr, partition_index + 1, end)
+def quick_sort(arr, low, high):
+    if low < high:
+        partition_index = partition(arr, low, high)
+        quick_sort(arr, low, partition_index - 1)
+        quick_sort(arr, partition_index + 1, high)
